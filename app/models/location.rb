5506bd7b6c8940921bcb7ae6b086292d227576fd
Maher Hawash <gmhawash@gmail.com>
2013-10-02 07:40:31 -0700

    Add iqama times for month view.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,7 @@
 class Location < ActiveRecord::Base
-  has_many :days
+  has_many :days do
+    def current_month
+      where(:date => Date.today.beginning_of_month..Date.today.next_month.beginning_of_month).order(:date)
+    end
+  end
 end
