4eb23d731325ee5008f76d9ddf0be5bf828e4fe9
Maher Hawash <gmhawash@gmail.com>
2013-10-05 05:45:47 +0100

    fix timezone issue

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,8 +1,13 @@
 class LocationsController < ApplicationController
+  def timezone
+    'Pacific Time (US & Canada)'
+  end
+
   def show
     @location = Location.find_by_nickname(params[:location])
+    @today = Time.now.in_time_zone(timezone).to_date
     if @location
-      @salat = @location.days.find_by_date(Date.today)
+      @salat = @location.days.find_by_date(@today)
     else
       raise ActionController::RoutingError.new('Not Found')
     end
