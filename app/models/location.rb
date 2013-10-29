9e26cf311c478e43b56f924983c57bdc5e3a97cf
Maher Hawash <gmhawash@gmail.com>
2013-10-29 07:22:50 -0700

    Start month view according to @today

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,7 @@
 class Location < ActiveRecord::Base
   has_many :days do
-    def current_month
-      where(:date => Date.today.beginning_of_month..Date.today.next_month.beginning_of_month).order(:date)
+    def current_month(starting_day = nil)
+      where(:date => starting_day.beginning_of_month..starting_day.next_month.beginning_of_month).order(:date)
     end
   end
 
